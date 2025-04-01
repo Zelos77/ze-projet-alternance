b0b81acf7c874ab27f91fc690d04c2385195cb23
Zelos77 <192414058+Zelos77@users.noreply.github.com>
2025-04-01 10:56:15 +0200

    Update ec2-userdata.sh
    Change git clone branch from a dev branch to main

--- setup/ec2-userdata.sh
+++ setup/ec2-userdata.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-apt update && apt install -y python3-pip nginx git unzip
-cd /home/ubuntu
 
-# Cloner ton projet GitHub
-git clone https://github.com/tonuser/projet-alternance.git
-cd projet-alternance/backend
+# Mise à jour des paquets
+sudo dnf update -y
+
+# Installer Git, Python3, pip, unzip, Nginx
+sudo dnf install -y git python3 python3-pip unzip nginx
+
+# Démarrer et activer nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+# Aller dans le dossier utilisateur
+cd /home/ec2-user
+
+# Cloner la branche de test du dépôt GitHub
+git clone --branch main https://github.com/Zelos77/ze-projet-alternance.git
+
+# Installer les dépendances du backend
+cd ze-projet-alternance/backend
 pip3 install -r requirements.txt
 
-# Lancer FastAPI
+# Lancer le backend FastAPI en arrière-plan
 nohup uvicorn main:app --host 0.0.0.0 --port 8000 &
 
-# Déployer le site HTML
-cp /home/ubuntu/projet-alternance/frontend/index.html /var/www/html/index.html
+# Copier le site web dans le dossier public nginx
+sudo cp ../frontend/index.html /usr/share/nginx/html/index.html
 
+# (Optionnel) Donner les bons droits
+sudo chown nginx:nginx /usr/share/nginx/html/index.html
