17ca71ceb53ed1b970ac77b2172e93ac5e0e3aef
Zelos77 <192414058+Zelos77@users.noreply.github.com>
2025-04-03 16:51:06 +0200

    add elb + rmeove useless sg rule (#7)

--- infra/securitygroup.tf
+++ infra/securitygroup.tf
@@ -13,14 +13,6 @@ resource "aws_vpc_security_group_egress_rule" "ssm_https_only" {
   description       = "Allow HTTPS egress for SSM Agent"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress_https" {
-  security_group_id = aws_security_group.ze_sg.id
-
-  cidr_ipv4   = "165.225.20.165/32"
-  from_port   = 443
-  to_port     = 443
-  ip_protocol = "tcp"
-}
 
 resource "aws_vpc_security_group_ingress_rule" "ingress_http" {
   security_group_id = aws_security_group.ze_sg.id
@@ -49,4 +41,13 @@ resource "aws_vpc_security_group_ingress_rule" "pub_sub_2_ssm" {
   from_port   = 443
   to_port     = 443
   cidr_ipv4   = aws_subnet.ze_pubsbn_2.cidr_block
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress_elb" {
+  security_group_id = aws_security_group.ze_sg.id
+
+  ip_protocol = "tcp"
+  from_port   = 8000
+  to_port     = 8000
+  cidr_ipv4   = "0.0.0.0/0"
 }
